4a1030d24f209ac808839b9a46f93a0dbce1980b
Brad Jones <brad@jones.name>
2015-10-31 03:36:45 -0600

    Add install script support

---
+++ src/Install.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SSPOpenIdConnect;
+
+class Install {
+  public static function postInstall() {
+    $reflector = new \ReflectionClass('SimpleSAML_Configuration');
+    $location = dirname(dirname(dirname($reflector->getFileName()))) . '/modules/openidconnect';
+    $file = $location . '/default-enable';
+    if (!file_exists($file)) {
+      mkdir($location, 0775);
+      touch($file);
+      mkdir($location . '/www', 0775);
+      copy(__FILE__ . '../www/resume.php', $location . '/www/resume.php');
+      // @todo - Throw an error here if we're unsuccessful?
+    }
+  }
+}
