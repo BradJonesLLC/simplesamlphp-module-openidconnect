1233062be99e1248479e6de1541130cbf3980eed
Kasper Rynning-Tønnesen <kasperr@stud.ntnu.no>
2016-07-19 14:22:22 +0200

    Update authsources-template-addition.php

--- template/authsources-template-addition.php
+++ template/authsources-template-addition.php
@@ -5,9 +5,9 @@ $config = array(
       'openidconnect:Connect',
       'client_id'           => '*****-*****-*****-*****-*****',
       'client_secret'       => '*****-*****-*****-*****-*****',
-      'token_endpoint'      => 'https://auth.dataporten.no/oauth/token',          //Example url
-      'user_info_endpoint'  => 'https://auth.dataporten.no/openid/userinfo',      //Example url
-      'auth_endpoint'       => 'https://auth.dataporten.no/oauth/authorization',  //Example url
+      'token_endpoint'      => 'https://openid.example.org/openid/token',          //Example url
+      'user_info_endpoint'  => 'https://openid.example.org/openid/userinfo',      //Example url
+      'auth_endpoint'       => 'https://openid.example.org/openid/authorization',  //Example url
       'sslcapath'           => '/etc/ssl/certs',
   ),
 
