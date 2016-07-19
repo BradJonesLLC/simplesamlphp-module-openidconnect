627228dd1081fcf6fbf883c2acac64acc508d9b9
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2016-07-19 12:55:32 +0200

    Added template for instruction on how to add config for easier understanding of adding the module

---
+++ template/authsources-template-addition.php
@@ -0,0 +1,13 @@
+<?php
+
+$config = array(
+  'dataporten-openid-connect' => array(
+      'dataportenopenidconnect:Connect',
+      'client_id'           => '*****-*****-*****-*****-*****',
+      'client_secret'       => '*****-*****-*****-*****-*****',
+      'token_endpoint'      => 'https://auth.dataporten.no/oauth/token',
+      'user_info_endpoint'  => 'https://auth.dataporten.no/openid/userinfo',
+      'auth_endpoint'       => 'https://auth.dataporten.no/oauth/authorization',
+  ),
+
+);
